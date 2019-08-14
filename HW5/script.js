eb4eddbbd21782182c5b19f69d3c23259b3fd43a
use <use@MacBook-Pro-user.local>
2019-08-14 21:30:28 +0300

    HW5

--- HW5/script.js
+++ HW5/script.js
@@ -1,30 +1,45 @@
 'use strict'
 
-let userStr = prompt('Please write your str', 'Hello world');
-userStr = userStr.toLowerCase();
-const l = 'l';
-const z = 'z';
-let userLetter1 = isCorrect(l);
-let userLetter2 = isCorrect(z);
+const messageLetter1 = 'Choose letter for search';
+const messageLetter2 = 'Choose letter for replace';
+const exampleLetter1 = 'l';
+const exampleLetter2 = 'z';
 
-function isCorrect(letter){
-    let userLetter = null;
-    do{
-        userLetter = prompt('Choose letter for ', letter);
+let userStr = checkUserStr();
+let userLetter1 = checkUserLetter(messageLetter1, exampleLetter1);
+let userLetter2 = checkUserLetter(messageLetter2, exampleLetter2);
+let newStr = replaceAll(userStr, userLetter1, userLetter2);
+
+alert('Your old str = ' + userStr + '\nYour new str = ' + newStr);
+console.log('Your old str = ' + userStr + '\nYour new str = ' + newStr);
+
+function checkUserStr() {
+    let checkedStr;
+    do {
+        checkedStr = prompt('Please write your str', 'Hello world');
     }
-    while(
+    while (
+        checkedStr == null ||
+        checkedStr.length == 0
+    )
+    return checkedStr;
+}
+
+function checkUserLetter(message, letter) {
+    let userLetter = null;
+    do {
+        userLetter = prompt(message, letter);
+    }
+    while (
+        userLetter == null ||
         userLetter.length > 1 ||
-        !isNaN(userLetter)
+        userLetter.length == 0
     )
     return userLetter;
 }
 
-let newStr = replaceAll(userStr, userLetter1, userLetter2);
-
-function replaceAll(userStr, userLetter1, userLetter2){
-    let c = null;
-    c = userStr.replace(userLetter1, userLetter2);
-    return c.indexOf(userLetter1) >= 0 ? replaceAll(c, userLetter1, userLetter2) : c;
+function replaceAll(userStr, userLetter1, userLetter2) {
+    let resultStr = null;
+    resultStr = userStr.replace(userLetter1, userLetter2);
+    return resultStr.indexOf(userLetter1) >= 0 ? replaceAll(resultStr, userLetter1, userLetter2) : resultStr;
 }
-
-console.log(newStr);
