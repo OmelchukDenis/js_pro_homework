8ceb32c60c978ba1c981beaa8461fc04922215b7
use <use@MacBook-Pro-user.local>
2019-08-30 17:11:35 +0300

    refactoring

--- HW7/script.js
+++ HW7/script.js
@@ -4,16 +4,17 @@ const QUESTION_FOR_NAME = 'Please, write your name.';
 const USER_NAME_EXAMPLE = 'User';
 const QUESTION_FOR_STR = 'Please, write string with number, like in example.';
 const USER_STR_EXAMPLE = '9,1,7,5,2,0';
-const ELEM_GREETING = document.getElementById('greeting');
-const ELEM_MIN = document.getElementById('min');
-const ELEM_MAX = document.getElementById('max');
+
+const elemGreeting = document.getElementById('greeting');
+const elemMin = document.getElementById('min');
+const elemMax = document.getElementById('max');
 
 let userName = getUserStr(QUESTION_FOR_NAME, USER_NAME_EXAMPLE)
 let userNumber = getUserStr(QUESTION_FOR_STR, USER_STR_EXAMPLE);
 let userNumberArray = userNumber.split(',').map(Number).sort(compareNumber);
-const LAST_INDEX = userNumberArray.length - 1;
+const lastIndex = userNumberArray.length - 1;
 
-if (isNaN(userNumberArray[LAST_INDEX])){
+if (isNaN(userNumberArray[lastIndex])){
     renderErrorPage()
 } else {
     renderPage()
@@ -32,17 +33,17 @@ function getUserStr(question, example) {
 }
 
 function renderPage(){
-    ELEM_GREETING.innerHTML = userName;
-    ELEM_MIN.innerHTML = userNumberArray[0];
-    ELEM_MAX.innerHTML = userNumberArray[LAST_INDEX];
+    elemGreeting.innerHTML = userName;
+    elemMin.innerHTML = userNumberArray[0];
+    elemMax.innerHTML = userNumberArray[lastIndex];
 }
 
 function renderErrorPage(){
-    ELEM_GREETING.innerHTML = userName;
-    ELEM_MIN.innerHTML = 'Error';
-    ELEM_MAX.innerHTML = 'Error';
-    ELEM_MIN.style.backgroundColor = 'red';
-    ELEM_MAX.style.backgroundColor = 'red';
+    elemGreeting.innerHTML = userName;
+    elemMin.innerHTML = 'Error';
+    elemMax.innerHTML = 'Error';
+    elemMin.style.backgroundColor = 'red';
+    elemMax.style.backgroundColor = 'red';
 }
 
 function compareNumber(a, b) {
