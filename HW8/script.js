90dda61b84644599e1c9a26f7aca757fd64520da
use <use@MacBook-Pro-user.local>
2019-08-31 12:49:28 +0300

    HW8

--- HW8/script.js
+++ HW8/script.js
@@ -4,9 +4,9 @@ const addBtn = document.getElementById('addBtn');
 const countElements = document.getElementById('count');
 const listOfElements = document.getElementById('list');
 
-addBtn.addEventListener('click', replaceElements);
+addBtn.addEventListener('click', replaceList);
 
-function replaceElements(){
+function replaceList(){
     listOfElements.innerHTML = '';
     for(let i = 1; i <= countElements.value; i++){
         const newElem = document.createElement('li');
