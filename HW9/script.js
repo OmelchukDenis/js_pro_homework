8c18b5a8c9ed181f090edfbe21bb1688ebf63c6f
use <use@MacBook-Pro-user.local>
2019-09-04 23:40:23 +0300

    Beautify code HW9

--- HW9/script.js
+++ HW9/script.js
@@ -8,47 +8,47 @@ const classError = 'error';
 document.getElementById('submitTask').addEventListener('submit', onFormSubmit);
 toDoList.addEventListener('click', onToDoListClick);
 
-function onFormSubmit(e){
+function onFormSubmit(e) {
     e.preventDefault();
     addNewTask();
 }
 
-function addNewTask(){
-    if(taskTitle.value.trim() !== ''){
+function addNewTask() {
+    if (taskTitle.value.trim() !== '') {
         deleteErrorClass(classError);
         toDoList.innerHTML += generateTask(taskTitle.value);
         taskTitle.placeholder = '';
         taskTitle.value = '';
-    }else{
+    } else {
         taskTitle.value = '';
         taskTitle.placeholder = 'Fill the input';
-        taskTitle.classList.add(classError);        
+        taskTitle.classList.add(classError);
     }
-    
+
 }
 
-function generateTask(value){
+function generateTask(value) {
     return itemTemplate.replace('{{item}}', value);
 }
 
-function onToDoListClick(e){
-    if(e.target.tagName == 'B'){
+function onToDoListClick(e) {
+    if (e.target.tagName == 'B') {
         removeLi(e);
-    }else if(e.target.tagName == 'LI'){
+    } else if (e.target.tagName == 'LI') {
         changeLiClass(e);
     }
 }
 
-function removeLi(e){
+function removeLi(e) {
     e.target.parentElement.remove();
 }
 
-function changeLiClass(e){
+function changeLiClass(e) {
     e.target.classList.toggle('doneTask');
 }
 
-function deleteErrorClass(classError){
-    if(taskTitle.classList.contains(classError)){
+function deleteErrorClass(classError) {
+    if (taskTitle.classList.contains(classError)) {
         taskTitle.classList.remove(classError);
     }
 }
