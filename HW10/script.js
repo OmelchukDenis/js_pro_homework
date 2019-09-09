3748927e6dc2cf195d543c4ec78120bd4e52100e
Denis <omelchuk.denis@gmail.com>
2019-09-09 13:04:26 +0300

    HW10

---
+++ HW10/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const calculator = createCalculator(10); 
+
+function createCalculator(number = 0){
+    let result = number;
+    return {
+        add: (arg) => result += arg,
+        sub: (arg) => result -= arg,
+        divide: (arg) => result /= arg,
+        mult: (arg) => result *= arg,
+        set: (arg) => result = arg
+    }
+} 
+
+console.log(calculator.add(45)); // возвращает 55 
+console.log(calculator.sub(45)); // возвращает 10 
+console.log(calculator.divide(5)); // возвращает 2 
+console.log(calculator.mult(5)); // возвращает 10
+console.log(calculator.set(100)); // устанавливает базовое значение в 100 
+console.log(calculator.mult(5)); // возвращает 500
