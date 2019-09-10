003a4a0bcaf749004cecb9e83b96560be2056f7e
use <use@MacBook-Pro-user.local>
2019-09-10 19:34:25 +0300

    HW11 refactoring

--- HW11/script.js
+++ HW11/script.js
@@ -16,6 +16,18 @@ Student.prototype.averageMark = function(){
 }
 
 const students = [ 
-    new Student('Student 1', [10,9,8,0,10]), // имя, оценки
-    new Student('Student 12', [10,0,8,0,3,4])
+    new Student('Student 1', [10,9,8,0,10]), // имя, оценки 7.4
+    new Student('Student 12', [10,0,8,0,3,4]) // 4.166
 ];
+
+averageMarkGroup();
+
+function averageMarkGroup(){
+    let studentsArr = students.length;
+    let total = 0;
+    for(let i = 0; i <= studentsArr - 1; i++){
+        total += students[i].averageMark();
+    }
+    const averageMarkGroup = total / studentsArr;
+    console.log(averageMarkGroup);
+}
