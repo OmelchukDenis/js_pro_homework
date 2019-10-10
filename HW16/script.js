57e40761643c70289a41c069072b9c805b474154
Denis <omelchuk.denis@gmail.com>
2019-10-10 21:11:44 +0300

    HW16

--- HW16/script.js
+++ HW16/script.js
@@ -1,48 +1,50 @@
 'use strict'
 
-const prom = fetch('https://jsonplaceholder.typicode.com/photos?_limit=50');
-const imageParent = document.getElementById("body");
-const bigImgTest = document.getElementById("bigImg");
-const imageGalery = document.createElement('div');
-imageParent.addEventListener('click', openBigImg);
-bigImgTest.addEventListener('click', hideElement);
+const ITEM_IMAGE_CLASS = 'item-image';
+const HIDE_ELEMENT_CLASS = 'hidden';
 
-prom.then((resp) => {
+const requestPhoto = fetch('https://jsonplaceholder.typicode.com/photos?_limit=50');
+const myGallery = document.getElementById('myGallery');
+const imageBig = document.getElementById('imageBig');
+const imageGallery = document.createElement('div');
+imageGallery.classList.add('imageGallery');
+
+imageGallery.addEventListener('click', showBigImg);
+imageBig.addEventListener('click', hideBigImg);
+
+requestPhoto.then((resp) => {
     resp.json().then((data) => {
-        runfunc(data);
+        createPhotoGallery(data);
     })
 });
 
-function runfunc(data){
-    for(var i = 0; i<=data.length-1; i++){
-        let d = {
-            dataUrl: data[i].thumbnailUrl,
-            dataBigUrl: data[i].url
-        };
-        createImageList(d);
+function createPhotoGallery(data){
+    const imageTag = createImageElement();
+    imageBig.appendChild(imageTag);
+    data.map(el => addImageInGallery(el.url, el.thumbnailUrl));
+    myGallery.appendChild(imageGallery);
+}
+
+function addImageInGallery(urlBigImg, urlSmallImg){
+    const image = createImageElement();
+    image.className = ITEM_IMAGE_CLASS;
+    image.src = urlSmallImg;
+    image.setAttribute('data-bigimg', urlBigImg);
+    imageGallery.appendChild(image);
+}
+
+function createImageElement(){
+    const imageElement = document.createElement('img');
+    return imageElement;
+}
+
+function showBigImg(e){
+    if(e.target.classList.contains(ITEM_IMAGE_CLASS)){
+        imageBig.querySelector('img').src = e.target.dataset.bigimg;
+        imageBig.classList.remove(HIDE_ELEMENT_CLASS);
     }
-    imageParent.appendChild(imageGalery);
 }
 
-function createImageList(d){
-    let image = document.createElement('img');
-    image.className = 'test';
-    image.src = d.dataUrl;
-    image.alt = d.dataBigUrl;
-    imageGalery.appendChild(image);
+function hideBigImg(){
+    imageBig.classList.add(HIDE_ELEMENT_CLASS);
 }
-
-function openBigImg(e){
-    const imgTest = e.target;
-    if(imgTest.classList.contains('test')){
-        const bigImg = document.createElement('img');
-        bigImg.src = imgTest.alt;
-        bigImgTest.appendChild(bigImg);
-    }
-}
-
-function hideElement(){
-    bigImgTest.innerHTML = '';
-}
-
-
