e1900e2ebd5f59f3bd171e1bc1a3205af052ee7a
use <use@MacBook-Pro-user.local>
2019-11-11 08:10:19 +0200

    hw24

---
+++ _HW24/gulpfile.js
@@ -0,0 +1,34 @@
+const { series, src, dest, watch } = require('gulp');
+var concat = require('gulp-concat');
+var inject = require('gulp-inject');
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
+
+function concatTask(cb){
+    src('./src/script/*.js')
+        .pipe(concat('main.js'))
+        .pipe(dest('./dist/script'));
+    cb();
+}
+
+function sassTask(cb){
+    src('./src/assets/scss/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(dest('./dist/css/'));
+    cb();
+}
+
+function injectTask(cb){
+    var target = src('./dist/index.html');
+    var sources = src(['./dist/**/*.js', './dist/**/*.css'], {read: false});
+
+    target.pipe(inject(sources))
+    .pipe(dest('./dist/'))
+
+    cb();
+}
+
+
+module.exports.concat = concatTask;
+module.exports.sass = sassTask;
+module.exports.inject = injectTask;
